cd65c65980c584ef54747d445cf2907eeba4d4d2
Olexander Hofman <oh@mamoru.network>
2023-05-19 13:37:26 +0200

    MAMORU-215: Ethereum full node, mamoru sniffer starred from block 1 - added syncing check before sniffer start - blockchain - light-chain - update bsc 1.2.2

--- eth/backend.go
+++ eth/backend.go
@@ -18,6 +18,7 @@
 package eth
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"math/big"
@@ -62,6 +63,9 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/rpc"
+
+	"github.com/ethereum/go-ethereum/mamoru"
+	"github.com/ethereum/go-ethereum/mamoru/mempool"
 )
 
 // Config contains the configuration options of the ETH protocol.
@@ -275,9 +279,9 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 	}
 
 	////////////////////////////////////////////////////////
-	//sniffer := mempool.NewSniffer(context.Background(), eth.txPool, eth.blockchain, chainConfig, mamoru.NewFeed(chainConfig))
+	sniffer := mempool.NewSniffer(context.Background(), eth.txPool, eth.blockchain, chainConfig, mamoru.NewFeed(chainConfig))
 
-	//go sniffer.SnifferLoop()
+	go sniffer.SnifferLoop()
 	////////////////////////////////////////////////////////
 
 	// Permit the downloader to use the trie cache allowance during fast sync
